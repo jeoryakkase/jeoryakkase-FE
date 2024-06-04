eae229e9216acbf19c2600a8d618aa8bf6f93718
[SW5] 백민혁 <minhyeokbaek100@gmail.com>
2024-06-05 01:08:21 +0900

    fix: UrlModal 사용예시

--- src/app/userinfo/[goalId]/modal/page.tsx
+++ src/app/userinfo/[goalId]/modal/page.tsx
@@ -2,12 +2,15 @@
 
 import { useEffect, useState } from "react";
 
+import { useRouter } from "next/navigation";
+
 import UrlModal from "@components/UrlModal";
 
 import UserInfoGoalPage from "../page";
 
 const UserInfoGoalPageModal = () => {
 	const [isOpen, setIsOpen] = useState(false);
+	const router = useRouter();
 
 	useEffect(() => {
 		setIsOpen(true);
@@ -15,7 +18,16 @@ const UserInfoGoalPageModal = () => {
 
 	return (
 		<>
-			{isOpen && <UrlModal />}
+			{isOpen && (
+				<UrlModal
+					title="제목"
+					button="확인"
+					buttonAction={() => router.back()}
+					closeButton="취소"
+				>
+					내용
+				</UrlModal>
+			)}
 			<UserInfoGoalPage />
 		</>
 	);
