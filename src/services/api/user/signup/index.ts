05ecdb57d901c403020f64af8b4cc0c141eb76a9
[SW5] 백민혁 <minhyeokbaek100@gmail.com>
2024-06-08 20:57:43 +0900

    fixes: 회원정보수정 작업중

--- src/services/api/user/signup/index.ts
+++ src/services/api/user/signup/index.ts
@@ -2,7 +2,7 @@ import { SignUpFormType } from "@containers/signup/signupValidation";
 import apiClient from "@lib/axiosConfig";
 
 const postSignUp = async (data: SignUpFormType) => {
-	return apiClient.post("/api/signup", data);
+	return apiClient.post("/signup", data);
 };
 
 export default postSignUp;
